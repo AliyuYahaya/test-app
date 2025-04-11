3b891d4f4e4117c062e952817c2d5cbf36bc9d23
Aliyu Yahaya <st10440112@vcconnect.edu.za>
2025-04-11 16:17:42 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>This is a test App</h1>
+        <p>It is a test for the Vite + React + TypeScript + Tailwind CSS + Vitest + React Testing Library template</p>
+      </div>
+    </>
+  )
+}
+
+export default App
